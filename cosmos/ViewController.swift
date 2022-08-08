63d433a8a125f5ef656ce11509c59a4ec866b978
Rafael Rollo <rafaelrollo92@gmail.com>
2022-08-08 17:24:57 +0100

    Initial commit

---
+++ cosmos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cosmos
+//
+//  Created by rafael.rollo on 08/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
